696485e23163f135337bd40a1dc4931e16be0e9d
Timothee Reynaud <reynaudtimothee@gmail.com>
2017-11-15 15:51:06 +0100

    pharmacy index integration

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -2,7 +2,15 @@ class PharmaciesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    # @pharmacies = Pharmacie.where.not(latitude: nil, longitude: nil)
+
     @pharmacies = Pharmacy.all
+
+    @hash = Gmaps4rails.build_markers(@pharmacies) do |pharmacie, marker|
+      marker.lat pharmacie.latitude
+      marker.lng pharmacie.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 
 end
