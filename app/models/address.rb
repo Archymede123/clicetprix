cb9439c3812f565cfd26c7ff193d51c56e4eeab0
Timothee Reynaud <reynaudtimothee@gmail.com>
2017-11-17 11:40:09 +0100

    added geocoding to addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,10 @@ class Address < ApplicationRecord
   validates :street, presence: true
   validates :zip_code, presence: true
   validates :city, presence: true
+  geocoded_by :full_address
+  after_validation :geocode, if: :address_changed?
+
+  def full_address
+    "#{street}, #{zip_code} #{city}"
+  end
 end
