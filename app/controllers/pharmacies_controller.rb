d397a6ae9e24f2c39d9f684a499b90fbc971ecb5
Timothee Reynaud <reynaudtimothee@gmail.com>
2017-11-20 18:43:30 +0100

    marker displaying on map

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -5,14 +5,19 @@ class PharmaciesController < ApplicationController
 
     address = params[:address]
     @pharmacies = Pharmacy.where.not(latitude: nil, longitude: nil).near(address, 10)
-
-
-
-    # @hash = Gmaps4rails.build_markers(@pharmacies) do |pharmacie, marker|
-    #   marker.lat pharmacie.latitude
-    #   marker.lng pharmacie.longitude
-    #   # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-    # end
+    @hash = Gmaps4rails.build_markers(@pharmacies) do |pharmacie, marker|
+      marker.lat pharmacie.latitude
+      marker.lng pharmacie.longitude
+      marker.infowindow "<p>#{pharmacie.name}
+       </p>
+      <a href='#{pharmacie.url}'>Voir le site</p>"
+      marker.json(id: pharmacie.id)
+      marker.picture(
+        url: ActionController::Base.helpers.asset_path("map_locator4.png"),
+        width: 33,
+        height: 49,
+      )
+    end
   end
 
 end
